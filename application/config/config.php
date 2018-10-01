b52b1be8be6799e757d007c0a62941bf65639f81
handesk <handeskim@users.noreply.github.com>
2018-10-02 00:18:31 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -48,11 +48,11 @@ $config['restore_key'] = 'hk@123';
 
 
 //////////chỉnh sửa///////
-$config['provide_service'] = 'http://45.77.12.144/';
-$config['secret_key'] = 'f1e0f5eb388c511e834e09b26b68f7c2';
+$config['provide_service'] = '';
+$config['secret_key'] = '';
 $config['api_name'] = 'API-DOICARD-V1';
-$config['username'] = 'Administrator';
-$config['password'] = 'admin@pay';
-$config['auth'] = '123123fF';
-$config['app_id'] = '5b82cf1058e693ca4d8b4573';
-$config['priv_key'] = '+Amgkh89z3IM4lBSQHfmlatNxGE2yftwvRjgEDSfzps8aWsRKM2G0LsMxUTxZtvOiByjcgorCZo7hXK5Y4lpIA==';
+$config['username'] = '';
+$config['password'] = '';
+$config['auth'] = '';
+$config['app_id'] = '';
+$config['priv_key'] = '+==';
